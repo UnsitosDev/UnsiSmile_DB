0541ef51fd5bd9a6318ad34bb4b9e6da417c92ab
fr0ste <73338az@gmail.com>
2024-02-25 14:31:13 -0600

    avance en historia clinica general: antecedentes personales no patologicos

--- schemas.sql
+++ schemas.sql
@@ -62,18 +62,19 @@ create table
         nacionalidad VARCHAR(100)
     );
 
-create table signos_vitales(
-id_signos_vitales int not null auto_increment primary key,
-peso FLOAT,
-estatura FLOAT,
-temperatura FLOAT,
-frecuencia_cardiaca FLOAT,
-frecuencia_respiratoria FLOAT,
-pesion_arterial FLOAT,
-saturacion_oxigeno FLOAT,
-glucosa FLOAT,
-pulso FLOAT
-);
+create table
+    signos_vitales (
+        id_signos_vitales int not null auto_increment primary key,
+        peso FLOAT,
+        estatura FLOAT,
+        temperatura FLOAT,
+        frecuencia_cardiaca FLOAT,
+        frecuencia_respiratoria FLOAT,
+        pesion_arterial FLOAT,
+        saturacion_oxigeno FLOAT,
+        glucosa FLOAT,
+        pulso FLOAT
+    );
 
 -- Crear la tabla Pacientes
 CREATE TABLE
@@ -107,35 +108,39 @@ CREATE TABLE
         FOREIGN KEY (FK_ResponsableID) REFERENCES Responsables (responsableID) ON DELETE CASCADE ON UPDATE CASCADE
     );
 
-
 /*INICIO DE ESQUEMAS DE HISTORIA CLINICA GENERAL*/
+create table
+    perfil_facial (
+        perfil_facilaID int AUTO_INCREMENT PRIMARY key,
+        perfil_facial VARCHAR(50)
+    );
 
-create table perfil_facial(
-    perfil_facilaID int AUTO_INCREMENT PRIMARY key,
-    perfil_facial VARCHAR(50)
-);
+create table
+    frente_facial (
+        frente_facilaID int AUTO_INCREMENT PRIMARY key,
+        frente_facial VARCHAR(50)
+    );
 
+-- Crear la tabla Examen Facial
+CREATE TABLE
+    examen_facial (
+        id_examen_facial BIGINT PRIMARY KEY AUTO_INCREMENT,
+        fk_perfil int,
+        fk_frente int, -- Ajustar según tu modelo de datos
+        senias_particulares TEXT,
+        FOREIGN KEY (fk_perfil) REFERENCES perfil_facial (perfil_facilaID),
+        FOREIGN KEY (fk_frente) REFERENCES frente_facial (frente_facilaID) -- Ajustar según tu modelo de datos
+    );
 
-create table frente_facial(
-    frente_facilaID int AUTO_INCREMENT PRIMARY key,
-    frente_facial VARCHAR(50)
-);
+CREATE TABLE
+    PreguntasAntecedentesHeredofamiliares (
+        preguntaID BIGINT AUTO_INCREMENT PRIMARY KEY,
+        pregunta VARCHAR(255)
+    );
 
-CREATE TABLE examen_facial (
-    id_examen_facial BIGINT PRIMARY KEY AUTO_INCREMENT,
-    fk_perfil int,
-    fk_frente int, -- ajusta según tu modelo de datos
-    senias_particulares TEXT,
-    FOREIGN KEY (fk_perfil) REFERENCES perfil_facial(perfil_facilaID),
-    FOREIGN KEY (fk_frente) REFERENCES frente_facial(frente_facilaID) -- ajusta según tu modelo de datos
-);
-
-CREATE TABLE PreguntasAntecedentesHeredofamiliares (
-    preguntaID BIGINT AUTO_INCREMENT PRIMARY KEY,
-    pregunta VARCHAR(255)
-);
-
-INSERT INTO PreguntasAntecedentesHeredofamiliares (pregunta) VALUES
+INSERT INTO
+    PreguntasAntecedentesHeredofamiliares (pregunta)
+VALUES
     ('Neoplasia (Cáncer)'),
     ('Diabetes'),
     ('Hipertensión Arterial'),
@@ -145,22 +150,55 @@ INSERT INTO PreguntasAntecedentesHeredofamiliares (pregunta) VALUES
     ('Malformaciones congénitas'),
     ('Problemas cardiacos');
 
-CREATE TABLE AntecedentesHeredofamiliares (
-    Antecedentes_heredofamiliaresID BIGINT PRIMARY KEY AUTO_INCREMENT,
-    preguntaID BIGINT,
-    respuesta_principal ENUM('si', 'no', 'ampliar') NOT NULL,
-    detalle_respuesta TEXT,
-    FOREIGN KEY (preguntaID) REFERENCES PreguntasAntecedentesHeredofamiliares(preguntaID) ON DELETE CASCADE ON UPDATE CASCADE
-);
+-- Crear la tabla Antecedentes Heredofamiliares
+CREATE TABLE
+    AntecedentesHeredofamiliares (
+        Antecedentes_heredofamiliaresID BIGINT PRIMARY KEY AUTO_INCREMENT,
+        preguntaID BIGINT,
+        respuesta_principal ENUM ('si', 'no', 'ampliar') NOT NULL,
+        detalle_respuesta TEXT,
+        FOREIGN KEY (preguntaID) REFERENCES PreguntasAntecedentesHeredofamiliares (preguntaID) ON DELETE CASCADE ON UPDATE CASCADE
+    );
 
-CREATE TABLE historias_clinicas (
-    historia_clinicaID BIGINT PRIMARY KEY AUTO_INCREMENT,
-    fk_examen_facial BIGINT,
-    fk_antecedentes_heredofamiliares BIGINT,
-    fk_paciente BIGINT,
-    FOREIGN KEY (fk_examen_facial) REFERENCES examen_facial(id_examen_facial),
-    FOREIGN KEY (fk_antecedentes_heredofamiliares) REFERENCES AntecedentesHeredofamiliares(Antecedentes_heredofamiliaresID),
-    FOREIGN KEY (fk_paciente) REFERENCES Pacientes(pacienteID)
-);
+-- Crear la tabla Material de Vivienda
+CREATE TABLE
+    material_vivienda (
+        material_vivienda_ID BIGINT PRIMARY KEY AUTO_INCREMENT,
+        material VARCHAR(100)
+    );
+
+CREATE TABLE
+    AP_no_patologicos (
+        ap_no_patologicosID BIGINT PRIMARY KEY AUTO_INCREMENT,
+        come_frutas_verduras BOOLEAN,
+        come_carnes BOOLEAN,
+        come_cereales BOOLEAN,
+        come_alimentos_chatarra BOOLEAN,
+        toma_agua_diario BOOLEAN,
+        toma_refrescos BOOLEAN,
+        horas_sueno_diarias INT,
+        veces_se_bana_semana INT,
+        veces_cepilla_dientes_dia INT,
+        vivienda_con_piso BOOLEAN,
+        fk_material_viviendaID BIGINT, -- Foreign key a material de vivienda
+        FOREIGN KEY (fk_material_viviendaID) REFERENCES material_vivienda (material_vivienda_ID) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+-- Crear la tabla Historias Clínicas
+CREATE TABLE
+    historias_clinicas (
+        historia_clinicaID BIGINT PRIMARY KEY AUTO_INCREMENT,
+        fk_paciente BIGINT,
+        fk_examen_facial BIGINT,
+        fk_antecedentes_heredofamiliares BIGINT,
+        fk_ap_no_patologicosID BIGINT, -- Foreign key a antecedentes personales no patologicos
+        FOREIGN KEY (fk_examen_facial) REFERENCES examen_facial (id_examen_facial) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (fk_antecedentes_heredofamiliares) REFERENCES AntecedentesHeredofamiliares (Antecedentes_heredofamiliaresID) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (fk_paciente) REFERENCES Pacientes (pacienteID) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (fk_ap_no_patologicosID) REFERENCES AP_no_patologicos (ap_no_patologicosID) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+
+
 
 /**INCIO DE ESQUEMAS PARA CONSULTAS*/
