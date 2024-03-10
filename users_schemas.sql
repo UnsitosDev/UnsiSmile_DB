e57670aae467047994657b649ab05d230b207ecb
fr0ste <73338az@gmail.com>
2024-03-10 13:00:53 -0600

    se crea la primera version del esquema de usuarios

---
+++ users_schemas.sql
@@ -0,0 +1,117 @@
+-- drop database unsis_smile;
+drop database if exists usuarios_unsis_smile;
+
+create database usuarios_unsis_smile;
+
+use usuarios_unsis_smile;
+
+-- ******************************************** ESQUEMAS PARA LOS USUARIOS (LOGUEO) *******************************
+-- Tabla tipos de roles
+CREATE TABLE
+    roles_tipos_usuario (
+        id_rol INT auto_increment PRIMARY KEY,
+        nombre_rol VARCHAR(50) NOT NULL
+    );
+
+-- Tabla usuario
+CREATE TABLE
+    usuarios (
+        id_usuario INT auto_increment PRIMARY KEY,
+        nombre_usuario VARCHAR(20) NOT NULL,
+        pass_usuario VARCHAR(15) NOT NULL,
+        f_id_rol int NOT NULL,
+        estatus boolean default true,
+        foreign key (f_id_rol) references roles_tipos_usuario (id_rol)
+    );
+
+-- ************************************ ESQUEMAS PARA LOS GRUPOS **************************************************
+create table
+    grupos (
+        id_grupo int auto_increment primary key,
+        grupo varchar(100),
+        semestre int
+    );
+
+create table
+    ciclos (
+        id_ciclo int AUTO_INCREMENT PRIMARY KEY,
+        ciclo varchar(100)
+    );
+
+-- ******************************************** ESQUEMAS DE USUARIOS DE LA APLICACION (usuarios finales)*******************************
+-- tablas administradores de clinica
+create table
+    personas (
+        id_persona int AUTO_INCREMENT PRIMARY key,
+        nombre1 varchar(50),
+        nombre2 varchar(50),
+        apellido1 varchar(50),
+        apellido2 varchar(50),
+        curp varchar(20),
+        telefono varchar(10),
+        fecha_nacimiento date,
+        sexo char, -- cambiar a entidad catálogo,
+        correo varchar(200)
+    );
+
+create table
+    administradores (
+        id_administrador int not null auto_increment primary key,
+        numero_trabajador varchar(15),
+        email_admin varchar(100) not NULL,
+        fk_persona int,
+        fk_usuario int,
+        foreign key (fk_persona) references personas (id_persona),
+        foreign key (fk_usuario) references usuarios (id_usuario)
+    );
+
+-- ************************datos de personal de salud***********************************
+create table
+    catedraticos (
+        id_catedratico int not null auto_increment primary key,
+        numero_trabajador varchar(15),
+        email_catedratico varchar(100) not NULL,
+        fk_usuario int,
+        fk_persona int,
+        foreign key (fk_persona) references personas (id_persona),
+        fk_id_usuario int not null,
+        FOREIGN KEY (fk_id_usuario) references usuarios (id_usuario)
+    );
+
+create table
+    semestres (
+        id_semestre int AUTO_INCREMENT PRIMARY KEY,
+        fk_grupo int,
+        fk_ciclo int,
+        foreign key (fk_grupo) references grupos (id_grupo),
+        foreign key (fk_ciclo) references ciclos (id_ciclo)
+    );
+
+create table
+    catedratico_grupo (
+        id_catedratico_semestre_grupo int not null auto_increment primary key,
+        fk_catedratico int,
+        fk_semestre int,
+        FOREIGN KEY (fk_catedratico) references catedraticos (id_catedratico),
+        FOREIGN KEY (fk_semestre) references semestres (id_semestre)
+    );
+
+create table
+    alumnos (
+        id_alumno int not null auto_increment primary key,
+        fk_id_semestre_grupo int,
+        matricula varchar(10),
+        fk_usuario int,
+        fk_persona int,
+        foreign key (fk_persona) references personas (id_persona),
+        FOREIGN KEY (fk_usuario) references usuarios (id_usuario)
+    );
+
+create table
+    alumno_semestre (
+        id_alumno_semestre int,
+        fk_alumno int,
+        fk_semestre int,
+        FOREIGN KEY (fk_alumno) references alumnos(id_alumno),
+        FOREIGN KEY (fk_semestre) references semestres(id_semestre)
+    );
