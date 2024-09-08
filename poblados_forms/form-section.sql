0626652bcc218bec7bd6f909f3547ec3e32ae73e
fr0ste <73338az@gmail.com>
2024-09-08 10:19:39 -0600

    Feat: se crear script para secciones de historia clínica general.

---
+++ poblados_forms/form-section.sql
@@ -0,0 +1,23 @@
+INSERT INTO form_sections
+(form_name)
+VALUES
+("Signos vitales"),
+("Exámen facial"),
+("Antecedentes Heredofamiliares"),
+("Antecedentes personales no patológicos"),
+("Antecedentes personales patológicos"),
+("Odontograma inicial"),
+("Odontograma final"),
+("Medicion de bolsas inicial"),
+("Exámen clínico"),
+("Análisis funcional"),
+("Postura del paciente"),
+("Postura del paciente"),
+("Exámen bucal"),
+("Análisis radigráfico"),
+("Modelos de estudio de fotografías"),
+("Estudio de laboratorio/biopsia"),
+("Interconsulta médica"),
+("Consentimiento informádo"),
+("Nota de evolución")
+;
